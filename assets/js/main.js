5945414b37ef160123a77ec636ed6bf313052894
kotsuma-yumiko <89885487+kotsuma-yumiko@users.noreply.github.com>
2021-09-24 11:30:39 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,7 +18,7 @@ const app = Vue.createApp({
       */
       correctAnswer: {
         stage1: {
-          q1: 'あああ',
+          q1: '6134572',
         },
         stage2: {
           q1: 'おもちゃ',
